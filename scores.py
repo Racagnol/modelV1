cc64d9b5012651609b11f37116a48da1f0271b93
Racagnol <racagnol@gmail.com>
2020-10-29 16:40:07 +0100

    Some feature

--- scores.py
+++ scores.py
@@ -13,7 +13,7 @@ class StudentsTestScore(Score):
 
     @classmethod
     def compute(cls, observation, prediction):
-        """Compute a t statistic and a p_value from an observation and a prediction."""
+        """Compute a t-statistic and a p_value from an observation and a prediction."""
         
 	p_mean = prediction['mean']  # Use the prediction's mean.
        	p_std = prediction['std']
